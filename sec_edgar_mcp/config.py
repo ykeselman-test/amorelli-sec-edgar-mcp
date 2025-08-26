0516526135a67859e53586adbdf03962b1ba06c7
Yakov Keselman <yakov.keselman@gmail.com>
2025-08-25 23:01:28 -0500

    Update config.py

--- sec_edgar_mcp/config.py
+++ sec_edgar_mcp/config.py
@@ -1,10 +1,10 @@
-import os
-
+# import os
 
 def initialize_config():
     """Initialize the SEC EDGAR configuration"""
-    sec_edgar_user_agent = os.getenv("SEC_EDGAR_USER_AGENT")
-    if not sec_edgar_user_agent:
-        raise ValueError("SEC_EDGAR_USER_AGENT environment variable is not set.")
+    sec_edgar_user_agent = "Yakov Keselman (yakov.keselman@gmail.com)"
+    # sec_edgar_user_agent = os.getenv("SEC_EDGAR_USER_AGENT")
+    # if not sec_edgar_user_agent:
+    #     raise ValueError("SEC_EDGAR_USER_AGENT environment variable is not set.")
 
     return sec_edgar_user_agent
